1fc95b89411ae4a80247ff078064c9c51f42c52e
Goldie <goldiejackson25@yahoo.com>
2021-10-02 16:40:47 -0500

    not complete

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 class Formatter {
-  //add static methods here
+  static capitalize(aString){
+    return aString.charAt(0).toUpperCase() + aString.slice(1);
+  }
+  static sanitize(aString){
+    return aString.replace(/[^A-Za-z0-9-']+/g, '');
+  }
+  static titleize(aString){
+//capitalizes all words in a sentence except the, a, an, but, of, and, for, at, by, and from; and always capitalizes the first word.
+  }
 }
